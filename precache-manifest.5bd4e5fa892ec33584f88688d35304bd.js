77938d789e027986a9400256529d9087514cd89a
Arne <arne.zerndt@student.hpi.de>
2019-05-16 16:31:26 +0200

    Updates

---
+++ precache-manifest.5bd4e5fa892ec33584f88688d35304bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f276e90b49b451ad9a7bc57ff8fc01",
+    "url": "/40k-calculator/index.html"
+  },
+  {
+    "revision": "4adc0ce4a1c1aba214bf",
+    "url": "/40k-calculator/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6523e13744b79580bee1",
+    "url": "/40k-calculator/static/css/main.53e3908f.chunk.css"
+  },
+  {
+    "revision": "4adc0ce4a1c1aba214bf",
+    "url": "/40k-calculator/static/js/2.01ee40f9.chunk.js"
+  },
+  {
+    "revision": "6523e13744b79580bee1",
+    "url": "/40k-calculator/static/js/main.97a6667b.chunk.js"
+  },
+  {
+    "revision": "78f9cb107fb96a3c76e7",
+    "url": "/40k-calculator/static/js/runtime~main.88df7287.js"
+  }
+]);
